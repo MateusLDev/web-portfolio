d35ff79b70888e4788bf44060b6bd5a31bb48194
Mateus Lopes <mateus.lopes_stormgroup@prestador.globo>
2024-07-08 21:32:11 -0300

    feature: Creating Button component

---
+++ components/Button.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+type ButtonVariantType = "primary" | "outlined" | "outlinded dark";
+
+enum buttonColors {
+  primary = "border-primary bg-primary text-white-900",
+  outlined = "border-primary bg-white-900 text-primary",
+  "outlinded dark" = "border-white-900 bg-black-800 text-white-900",
+}
+
+interface ButtonPropsInterface {
+  text: string;
+  variant: ButtonVariantType;
+}
+
+const { text, variant } = defineProps<ButtonPropsInterface>();
+
+const buttonStyle = computed(() => {
+  return buttonColors[variant];
+});
+</script>
+
+<template>
+  <button
+    class="border px-4 py-2 rounded-md font-medium text-xs"
+    :class="buttonStyle"
+  >
+    {{ text }}
+  </button>
+</template>
