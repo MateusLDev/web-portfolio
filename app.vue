e783c110189b4c6220116b783a5a9de20189bcb8
Mateus Lopes <mateus.lopes_stormgroup@prestador.globo>
2024-07-10 23:14:49 -0300

    Feature: using AOS plugin to animate Home

--- app.vue
+++ app.vue
@@ -1,15 +1,23 @@
+<script setup lang="ts">
+import AOS from "aos";
+
+onMounted(() => {
+    AOS.init();
+})
+</script>
+
 <template>
   <video
     autoplay
     muted
     loop
     id="myVideo"
-    class="fixed right-0 bottom-0 w-full opacity-[0.04] object-cover"
+    class="absolute right-0 bottom-0 w-full opacity-[0.04] object-cover"
   >
     <source src="./assets/light-background.mp4" type="video/mp4" />
   </video>
 
-  <div class="container mx-auto px-4">
+  <div class="container mx-auto px-4 h-full">
     <NuxtPage class="h-dvh relative z-20" />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,38 @@
 <template>
-  <div class="h-dvh flex flex-col items-center justify-center px-4">
-    <span class="text-black-700 text-base md:text-xl font-medium">Hi, I’m Mavue 💪</span>
+  <div class="h-dvh flex flex-col items-center justify-center px-4 ove">
+    <span
+      class="text-black-700 text-base md:text-xl font-medium"
+      data-aos="fade-up"
+      data-aos-duration="1000"
+      >Hi, I’m Mavue 💪</span
+    >
 
-    <h1 class="text-black-800 text-5xl font-bold text-center uppercase mt-2 mb-4">
+    <h1
+      class="text-black-800 text-5xl font-bold text-center uppercase mt-2 mb-4"
+      data-aos="fade-up"
+      data-aos-duration="1300"
+    >
       Frontend developer
     </h1>
 
-    <p class="text-black-600 text-sm md:text-base text-center">
+    <p
+      class="text-black-600 text-sm md:text-base text-center"
+      data-aos="fade-up"
+      data-aos-duration="1800"
+    >
       I’m Mateus Lopes, a passionate
       <span class="font-medium">frontend developer </span> <br />
       an <span class="font-medium">UI/UX designer</span> based in Rio de
       Janeiro, Brazil
     </p>
 
-    <div class="flex gap-3 md:gap-4 mt-6">
-    <Button text="Get in touch" variant="primary" />
-    <Button text="Download CV" variant="outlined" />
+    <div
+      class="flex gap-3 md:gap-4 mt-6"
+      data-aos="fade-up"
+      data-aos-duration="2000"
+    >
+      <Button text="Get in touch" variant="primary" />
+      <Button text="Download CV" variant="outlined" />
     </div>
   </div>
 </template>
